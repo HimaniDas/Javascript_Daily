792ba0548be5ea5103293bf91e8375139df9465c
HimaniDas <himani.das1998@gmail.com>
2025-04-03 23:14:49 +0600

    updated calculation of greatest value

--- the_greatest.js
+++ the_greatest.js
@@ -1,3 +1,16 @@
+//Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
+
+//Input
+//The input file contains 3 integer values.
+
+//Output
+//Print the greatest of these three values followed by a space and the message “eh o maior”.
+
+//Input Samples	
+//7 14 106
+//Output Samples
+//106 eh o maior
+
 var input = require('fs').readFileSync('input.txt', 'utf8');
 var lines = input.split('\n');
 var [a, b, s] = lines[0].split(" ").map(Number);
