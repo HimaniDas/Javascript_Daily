1910c6d259b9f90ded30bb85fc91ed8bdcdcfa76
HimaniDas <himani.das1998@gmail.com>
2025-05-04 23:03:54 +0600

    added the calculation of coordinates of a point

--- coordinates_of_a_point.js
+++ coordinates_of_a_point.js
@@ -19,11 +19,11 @@ var input = require('fs').readFileSync('input.txt', 'utf8');
 var lines = input.split('\n');
 var [x, y] = lines[0].split(' ').map(Number);
 
-if (x == 0 && y == 0) {
+if (x == 0 && y == 0) { //if (x === 0 && y === 0) {
     console.log("Origem");
-} else if (x == 0) {
+} else if (x == 0) { //} else if (x == 0) {
     console.log("Eixo Y");
-} else if (y == 0) {
+} else if (y == 0) { //} else if (y == 0) {
     console.log("Eixo X");
 } else if (x > 0 && y > 0) {
     console.log("Q1");
