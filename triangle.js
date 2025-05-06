5e75b318da32fde587b79014b1bc66044517e710
HimaniDas <himani.das1998@gmail.com>
2025-05-06 23:49:03 +0600

    triangle calculation

--- triangle.js
+++ triangle.js
@@ -1,3 +1,21 @@
+//Read three point floating values (A, B and C) and verify if is possible to make a triangle with them. If it is possible, calculate the perimeter of the triangle and print the message:
+
+//Perimetro = XX.X
+
+//If it is not possible, calculate the area of the trapezium which basis A and B and C as height, and print the message:
+//Area = XX.X
+
+//Input
+//The input file has tree floating point numbers.
+
+//Output
+//Print the result with one digit after the decimal point.
+
+//Input Sample	
+//6.0 4.0 2.0
+//Output Sample
+//Area = 10.0
+
 var input = require('fs').readFileSync('input.txt', 'utf8');
 var lines = input.split('\n');
 let [A, B, C] = lines[0].split(' ').map(Number);
