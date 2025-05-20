23810d59df71f5912a66957fdf2d21b44c1517f1
HimaniDas <himani.das1998@gmail.com>
2025-05-20 23:03:54 +0600

    Updated game time calculation

--- game_time.js
+++ game_time.js
@@ -1,3 +1,16 @@
+//Read the start time and end time of a game, in hours. Then calculate the duration of the game, knowing that the game can begin in a day and finish in another day, with a maximum duration of 24 hours. The message must be printed in portuguese “O JOGO DUROU X HORA(S)” that means “THE GAME LASTED X HOUR(S)”
+
+//Input
+//Two integer numbers representing the start and end time of a game.
+
+//Output
+//Print the duration of the game as in the sample output.
+
+//Input Sample	
+//16 2
+//Output Sample
+//O JOGO DUROU 10 HORA(S)
+
 var input = require('fs').readFileSync('input.txt', 'utf8');
 var lines = input.split('\n');
 let [start, end] = lines[0].split(' ').map(Number);//This splits the input string into two parts: "16" and "2", then converts them to numbers.
