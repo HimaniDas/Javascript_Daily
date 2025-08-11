753dc86081b48c31d9dfc77459d5ef626da28c97
HimaniDas <himani.das1998@gmail.com>
2025-08-11 20:31:19 +0600

    Updated Summing consecutive intergers

--- summing_consecutive_intergers.js
+++ summing_consecutive_intergers.js
@@ -1,3 +1,18 @@
+/*Write an algorithm to read a value A and a value N. Print the sum of N numbers from A (inclusive). While N is negative or ZERO, a new N (only N) must be read. All input values are in the same line.
+
+Input
+The input contains only integer values, ​​can be positive or negative.
+
+Output
+The output contains only an integer value.
+
+Input Sample	
+3 2
+
+Output Sample
+7
+*/
+
 var input = require('fs').readFileSync('input.txt', 'utf8').trim().split(' ').map(Number);
 
 let A = input[0];
